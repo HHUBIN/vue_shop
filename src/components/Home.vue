941b2a63964b1060af49bc79ad2ea9bd65fb0246
hubin <1426261937@qq.com>
2020-03-11 21:05:25 +0800

    登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button @click="outLogin">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    outLogin: function() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import './assets/css/global.css'
+import { Button, Form, FormItem, Input } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import { Message } from 'element-ui'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 Vue.config.productionTip = false
 
